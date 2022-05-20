95e845318f43473148ae348d1a4ef66899797e3f
saidReclip <said@reclip.app>
2022-05-20 12:46:12 +0300

    Initial Commit

---
+++ engenious-challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  engenious-challenge
+//
+//  Created by Abdullah Atkaev on 20.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ engenious-challengeUITests/engenious_challengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  engenious_challengeUITestsLaunchTests.swift
+//  engenious-challengeUITests
+//
+//  Created by Abdullah Atkaev on 20.05.2022.
+//
+
+import XCTest
+
+class engenious_challengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
