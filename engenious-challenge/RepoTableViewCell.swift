e63f46c6c042c276b7859f3d09e48b89cecedb80
ANTONB02 <bugera.anton@kingfisher.com>
2022-06-03 00:59:46 +0100

    [TST-003] Refactor design

--- engenious-challenge/RepoTableViewCell.swift
+++ engenious-challenge/RepoTableViewCell.swift
@@ -7,21 +7,129 @@
 
 import UIKit
 
-class RepoTableViewCell: UITableViewCell {
-    let titleLabel: UILabel = .init()
+//MARK: - Config
+fileprivate struct Config {
+    static let gradientBackgroundGaps = UIEdgeInsets(top: 8.0, left: 20.5, bottom: -8.0, right: -20.5)
+    static let titleLabelGaps = UIEdgeInsets(top: 24.0, left: 36.5, bottom: 8.0, right: -36.5)
+    static let descriptionLabelGaps = UIEdgeInsets(top: 8.0, left: 36.5, bottom: -24.0, right: -36.5)
+    
+    static let gradientBackgroundColors = [
+        UIColor(named: "gradientBackgroundLeft")?.cgColor ?? UIColor(red: 0.838, green: 0.933, blue: 1, alpha: 1).cgColor,
+        UIColor(named: "gradientBackgroundRight")?.cgColor ?? UIColor(red: 0.717, green: 0.881, blue: 1, alpha: 0.76).cgColor
+    ]
+    static let gradientBackgroundLocations: [NSNumber] = [0, 1]
+    static let gradientBackgroundStartPoint = CGPoint(x: 0.25, y: 0.5)
+    static let gradientBackgroundEndPoint = CGPoint(x: 0.75, y: 0.5)
+    
+    static let shadowColor = UIColor(named: "gradientBackgroundShadow")?.cgColor ?? UIColor(red: 0, green: 0.417, blue: 0.716, alpha: 0.08).cgColor
+    static let shadowOpacity: Float = 1
+    static let shadowOffset = CGSize(width: 0, height: 6)
+    static let shadowRadius: CGFloat = 6
+    static let shadowCornerRadius: CGFloat = 10
+    
+    static let cornerRadius: CGFloat = 10
+    
+    static let titleLabelFont = UIFont.systemFont(ofSize: 18.0, weight: .bold)
+    static let titleLabelColor = UIColor(named: "cellTitle") ?? UIColor(red: 0, green: 0.417, blue: 0.716, alpha: 1)
+    
+    static let descriptionLabelFont = UIFont.systemFont(ofSize: 14.0, weight: .medium)
+    static let descriptionLabelColor = UIColor(named: "cellDescription") ?? UIColor(red: 0, green: 0.283, blue: 0.487, alpha: 1)
+}
+
+// MARK: - Cell
+final class RepoTableViewCell: UITableViewCell {
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = Config.titleLabelFont
+        label.textColor = Config.titleLabelColor
+        return label
+    }()
+    
+    lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.font = Config.descriptionLabelFont
+        label.textColor = Config.descriptionLabelColor
+        return label
+    }()
+    
+    private lazy var gradientLayer: CAGradientLayer = {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = Config.gradientBackgroundColors
+        gradientLayer.locations = Config.gradientBackgroundLocations
+        gradientLayer.startPoint = Config.gradientBackgroundStartPoint
+        gradientLayer.endPoint = Config.gradientBackgroundEndPoint
+        gradientLayer.cornerRadius = Config.cornerRadius
+        return gradientLayer
+    }()
+    
+    private lazy var shadowLayer: CALayer = {
+        let shadowLayer = CALayer()
+        shadowLayer.backgroundColor = UIColor.white.cgColor
+        shadowLayer.shadowColor = Config.shadowColor
+        shadowLayer.shadowOpacity = Config.shadowOpacity
+        shadowLayer.shadowOffset = Config.shadowOffset
+        shadowLayer.shadowRadius = Config.shadowRadius
+        shadowLayer.cornerRadius = Config.shadowCornerRadius
+        return shadowLayer
+    }()
+    
+    private lazy var gradientBackgroundView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.insertSublayer(shadowLayer, at: 0)
+        view.layer.insertSublayer(gradientLayer, at: 1)
+        return view
+    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        contentView.addSubview(gradientBackgroundView)
         contentView.addSubview(titleLabel)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16).isActive = true
-        titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16).isActive = true
-        titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16).isActive = true
-        titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16).isActive = true
+        contentView.addSubview(descriptionLabel)
+        
+        setConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        updateGradientAndShadow()
+    }
+    
+    private func updateGradientAndShadow() {
+        gradientLayer.frame = gradientBackgroundView.bounds
+        shadowLayer.frame = gradientBackgroundView.bounds
+    }
+    
+    private func setConstraints() {
+        let gradientBackgroundViewConstraints: [NSLayoutConstraint] = [
+            gradientBackgroundView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Config.gradientBackgroundGaps.left),
+            gradientBackgroundView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: Config.gradientBackgroundGaps.top),
+            gradientBackgroundView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: Config.gradientBackgroundGaps.right),
+            gradientBackgroundView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: Config.gradientBackgroundGaps.bottom)
+        ]
+        
+        let titleLabelConstraints: [NSLayoutConstraint] = [
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: Config.titleLabelGaps.top),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Config.titleLabelGaps.left),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: Config.titleLabelGaps.right)
+        ]
+        
+        let descriptionLabelConstraints: [NSLayoutConstraint] = [
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: Config.descriptionLabelGaps.top),
+            descriptionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Config.descriptionLabelGaps.left),
+            descriptionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: Config.descriptionLabelGaps.right),
+            descriptionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: Config.descriptionLabelGaps.bottom)
+        ]
+        
+        let constraints: [NSLayoutConstraint] = gradientBackgroundViewConstraints + titleLabelConstraints + descriptionLabelConstraints
+        NSLayoutConstraint.activate(constraints)
     }
 }
--- engenious-challenge/RootViewController.swift
+++ engenious-challenge/RootViewController.swift
@@ -7,23 +7,51 @@
 
 import UIKit
 import Combine
+// MARK: - Config
+fileprivate struct Config {
+    static let gradientBackgroundColors = [
+        UIColor(named: "backgroundLeft")?.cgColor ?? UIColor(red: 0.996, green: 0.996, blue: 0.996, alpha: 1).cgColor,
+        UIColor(named: "backgroundRight")?.cgColor ?? UIColor(red: 0.961, green: 0.961, blue: 0.961, alpha: 1).cgColor
+    ]
+    static let gradientBackgroundLocations: [NSNumber] = [0, 1]
+    static let gradientBackgroundStartPoint = CGPoint(x: 0.25, y: 0.5)
+    static let gradientBackgroundEndPoint = CGPoint(x: 0.75, y: 0.5)
+    
+    static let sectionHeaderFont = UIFont.systemFont(ofSize: 20.0, weight: .semibold)
+    static let sectionHeaderColor = UIColor(named: "sectionHeader") ?? UIColor(red: 0, green: 0.417, blue: 0.716, alpha: 1)
+    static let sectionHeaderText = "Repositories"
+    static let sectionHeaderLeading: CGFloat = 20.5
+    static let sectionHeaderHeight: CGFloat = 56.0
+}
 
-class RootViewController: UIViewController {
+// MARK: - ViewController
+final class RootViewController: UIViewController {
     let repositoryService: RepositoryService = RepositoryService()
     let userName: String = "Apple"
-    let tableView = UITableView()
+    let tableView = UITableView(frame: .zero, style: .grouped)
     
     var repoList: [Repo] = []
     
+    private lazy var gradientLayer: CAGradientLayer = {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = Config.gradientBackgroundColors
+        gradientLayer.locations = Config.gradientBackgroundLocations
+        gradientLayer.startPoint = Config.gradientBackgroundStartPoint
+        gradientLayer.endPoint = Config.gradientBackgroundEndPoint
+        return gradientLayer
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.layer.insertSublayer(gradientLayer, at: 0)
         
         title = "\(userName)'s repos"
         navigationController?.navigationBar.prefersLargeTitles = true
         
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .clear
         tableView.register(RepoTableViewCell.self, forCellReuseIdentifier: String(describing: RepoTableViewCell.self))
         tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
@@ -33,6 +61,12 @@ class RootViewController: UIViewController {
         tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         
         getRepos()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        gradientLayer.frame = view.bounds
     }
     
     private func getRepos() {
@@ -55,6 +89,26 @@ extension RootViewController: UITableViewDelegate, UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RepoTableViewCell.self)) as? RepoTableViewCell else { return UITableViewCell() }
         let repo = repoList[indexPath.row]
         cell.titleLabel.text = repo.name
+        cell.descriptionLabel.text = repo.description
+        cell.backgroundColor = .clear
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let frame = CGRect(origin: CGPoint(x: Config.sectionHeaderLeading, y: 0),
+                           size: CGSize(width: tableView.bounds.width, height: Config.sectionHeaderHeight))
+        
+        let label = UILabel(frame: frame)
+        label.font = Config.sectionHeaderFont
+        label.textColor = Config.sectionHeaderColor
+        label.text = Config.sectionHeaderText
+        
+        let view = UIView()
+        view.addSubview(label)
+        return view
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return Config.sectionHeaderHeight
     }
 }
